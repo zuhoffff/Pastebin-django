384c7159eac8adc693126eccb03cf580c2882ab6
Nick <snicky5746@gmail.com>
2024-05-30 16:24:23 +0200

    Working on custom hash server

--- main_app/pastebin_main_app/models.py
+++ main_app/pastebin_main_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Metadata(models.Model):
     timestamp = models.CharField(max_length=150, unique=False, null=False)
     user_agent = models.CharField(max_length=150, unique=False, null=False)
-    s3_key = models.CharField(max_length=10, unique=True, null=False)
+    s3_key = models.CharField(max_length=10, unique=True, null=False)   
     key_usages = models.IntegerField(unique=False, null=False, default=0)
 
     def __str__(self):
