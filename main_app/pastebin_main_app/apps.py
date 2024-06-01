ef674abccc597dda40ba47f635fafdda98709f4d
Nick <snicky5746@gmail.com>
2024-06-01 21:35:29 +0200

    this expiry controller seems to work fine, tho needs to be tested more, and have more logging, error handing

--- main_app/pastebin_main_app/apps.py
+++ main_app/pastebin_main_app/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+from .expiry_controller import start_expiry_controller
 
 class PastebinMainAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pastebin_main_app'
+
+    def ready(self):
+        start_expiry_controller()
