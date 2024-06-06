7aa74daa98cce5ce97b6de750bd369fd921a40ba
Nick <snicky5746@gmail.com>
2024-06-06 20:25:30 +0200

    Expiry controller class works fine now

--- main_app/pastebin_main_app/expiry_controller.py
+++ main_app/pastebin_main_app/expiry_controller.py
@@ -2,6 +2,10 @@ import time
 from threading import Thread, Event
 from pastebin_main_app.myUtilFunctions import insert_to_sorted_list_returning_position
 from typing import Callable
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+LOGGER = logging.getLogger(__name__)
 
 class ExpiryController:
     # I was in two minds whether to us DI or pass the function and desided to stick with the second option
@@ -33,14 +37,16 @@ class ExpiryController:
         Returns:
             None
         """
-
         new_event = (expiry_time, id)
         insertion_point = insert_to_sorted_list_returning_position(self.expiry_registry, new_event)
+        LOGGER.info(f'Entry added {expiry_time} -- {id}')
 
         if insertion_point == 0:
             self.expiry_event.set()
 
     def run_expiry_controller(self):
+        LOGGER.info('The expiry controller started')
+        LOGGER.info(len(self.expiry_registry))
         while True:
             if not self.expiry_registry:
                 self.expiry_event.wait()
@@ -52,9 +58,10 @@ class ExpiryController:
             # Make sure both are float:
             time_til_next_expiry = self.expiry_registry[0][0] - current_time
             
+            # LOGGER.info('Performing a check: ')
             if time_til_next_expiry <= 0:
-                t = Thread(target=self.delete_expired_entry, args=(self.expiry_registry[0][1],))
-                t.start()
+                deletionThread = Thread(target=self.delete_expired_entry, args=(self.expiry_registry[0][1],))
+                deletionThread.start()
                 self.expiry_registry.pop(0)
             else:
                 self.expiry_event.wait(timeout=time_til_next_expiry)
