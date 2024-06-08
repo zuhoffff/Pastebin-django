eaae55663b87890d0f9309a5615914d6864ea9c8
Nick <snicky5746@gmail.com>
2024-06-08 18:25:17 +0200

    Classes view and service need refactoring

--- main_app/pastebin_main_app/models.py
+++ main_app/pastebin_main_app/models.py
@@ -10,7 +10,7 @@ class Metadata(models.Model):
     password=models.CharField(max_length=150, unique=False, null=True)
 
     def __str__(self):
-        return self.timestamp
+        return self.url
 
     class Meta:
         db_table = 'metadata'
--- main_app/pastebin_main_app/submit_text/submition_form.py
+++ main_app/pastebin_main_app/submit_text/submition_form.py
@@ -4,8 +4,8 @@ from pastebin_main_app.models import Metadata
 class PasteSubmissionForm(forms.ModelForm):
     # Field that not related to db:
     text = forms.CharField(max_length=10000, required=True)
+    password = forms.CharField(max_length=150, required=True)
 
     class Meta:
         model = Metadata
-        fields = ['author',  'password']
-        
+        fields = ['author']
