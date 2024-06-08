e11af802f7689e7b0718b4c52a9124d21cd8a273
Nick <snicky5746@gmail.com>
2024-06-08 18:25:17 +0200

    Classes view and service need refactoring

--- main_app/pastebin_main_app/utils/s3_handler.py
+++ main_app/pastebin_main_app/utils/s3_handler.py
@@ -7,48 +7,42 @@ from botocore.exceptions import NoCredentialsError, PartialCredentialsError, Cli
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 LOGGER = logging.getLogger(__name__)
 
+class S3Service():
+    # Constants for error messages
+    ERROR_CREDENTIALS = 'Credentials error'
+    ERROR_CLIENT = 'Client error'
+    ERROR_GENERIC = 'An error occurred'
+
+    def __init__(self, aws_access_key_id, aws_secret_access_key, bucket_name) -> None:
+        self.s3 = boto3.client('s3',
+                               aws_access_key_id,
+                               aws_secret_access_key)
+        self.bucket_name = bucket_name
+    
+    def upload_to_s3(self, s3_key, text_input):
+        try:
+            self.s3.put_object(Bucket=self.bucket_name, Key=str(s3_key), Body=str(text_input))
+            LOGGER.info('Object uploaded to S3')
+        except (NoCredentialsError, PartialCredentialsError) as e:
+            LOGGER.error(f'{S3Service.ERROR_CREDENTIALS}: {e}')
+            raise
+        except ClientError as e:
+            LOGGER.error(f'{S3Service.ERROR_CLIENT}: {e}')
+            raise
+        except Exception as e:
+            LOGGER.error(f'{S3Service.ERROR_GENERIC}: {e}')
+            raise
+
+    def retrieve_from_s3(self, s3_key):
+        s3_object = self.s3.get_object(Bucket=self.bucket_name, Key=s3_key)
+        return s3_object['Body'].read().decode('utf-8')
+
+    def delete_from_s3(self, s3_key):
+        self.s3.delete_object(Bucket=self.bucket_name, Key=s3_key)
+        LOGGER.info('Object deleted')
+
 key_id = environ.get('aws_access_key_id')
 secret_key = environ.get('aws_secret_access_key')
 BUCKET_NAME=environ.get('BUCKET_NAME')
 
-s3=boto3.client('s3',
-                aws_access_key_id=key_id,
-                aws_secret_access_key=secret_key)
-
-# Check if the S3 connection is successful
-try:
-    s3.list_buckets()
-    LOGGER.info('Connected to S3 successfully.')
-except Exception as e:
-    LOGGER.error('Failed to connect to S3:', exc_info=True)
-
-# Constants for error messages
-ERROR_HASH_SERVER = 'Hash-server error'
-ERROR_CREDENTIALS = 'Credentials error'
-ERROR_CLIENT = 'Client error'
-ERROR_GENERIC = 'An error occurred'
-ERROR_MISSING_DATA = 'Missing data'
-ERROR_INVALID_METHOD = 'Invalid request method'
-
-def upload_to_s3(s3_key, text_input):
-    try:
-        s3.put_object(Bucket=BUCKET_NAME, Key=str(s3_key), Body=str(text_input))
-        LOGGER.info('Object uploaded to S3')
-    except (NoCredentialsError, PartialCredentialsError) as e:
-        LOGGER.error(f'{ERROR_CREDENTIALS}: {e}')
-        raise
-    except ClientError as e:
-        LOGGER.error(f'{ERROR_CLIENT}: {e}')
-        raise
-    except Exception as e:
-        LOGGER.error(f'{ERROR_GENERIC}: {e}')
-        raise
-
-def retrieve_from_s3(s3_key):
-    s3_object = s3.get_object(Bucket=BUCKET_NAME, Key=s3_key)
-    return s3_object['Body'].read().decode('utf-8')
-
-def delete_from_s3(s3_key):
-    s3.delete_object(Bucket=BUCKET_NAME, Key=s3_key)
-    LOGGER.info('Object deleted')
-    return 0
+myS3Service = S3Service(key_id, secret_key, BUCKET_NAME)
--- main_app/pastebin_main_app/views.py
+++ main_app/pastebin_main_app/views.py
@@ -1,3 +1,4 @@
+import dateutil.parser as dp
 from django.forms import BaseModelForm
 from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -16,28 +17,49 @@ from models import Metadata
 from .. import SubmitTextService_MixinStatic
 
 @csrf_exempt
-class SubmitTextView(CreateView, SubmitTextService_MixinStatic):
+class SubmitTextView(CreateView):
     model = Metadata
     template_name='home.html'
     form_class = PasteSubmissionForm
     success_url = ...
 
-    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+    def __init__(self, submit_text_service_object, s3_service, **kwargs: logging.Any) -> None:
+        super().__init__(**kwargs)
+        self.submit_text_service=submit_text_service_object
+        self.s3_service = s3_service
+
+    def form_valid(self, form) -> HttpResponse:
+        text = form.cleaned_data.get('text')
+
         nonform_data = self.request.POST
-        nonform_data.get('text')
-        nonform_data.get('timestamp') # Needs to be converted to UNIX from iso
-        nonform_data.get('expirationTime') # Needs to be converted to Unix and added to timestamp
+        timestamp = nonform_data.get('timestamp') # Needs to be converted to UNIX from iso
+        time_til_expiry = nonform_data.get('expirationTime') # Needs to be converted to Unix and added to timestamp
+        user_agent = nonform_data.get('userAgent')
 
-        # Make request to hash-server by calling static method:
-        SubmitTextService_MixinStatic.get_hash_from_server()
+        # Make request to hash-server
+        url = self.submit_text_service.get_hash_from_server()
 
+        # Make necessary convertations:
+        timestamp = self.submit_text_service.convert_iso_to_unix(timestamp)
+        expiry_time = self.submit_text_service.convert_string_to_unix_time(time_til_expiry) + timestamp 
 
-        return super().form_valid(form)
+        # Process the validated form data but don't save to the database yet
+        new_entry = form.save(commit=False)
+        new_entry.timestamp = timestamp
+        new_entry.expiry_time = expiry_time
+        new_entry.password = make_password(form.password)
+        new_entry.user_agent = user_agent
+        new_entry.url = url
 
-    # def post(self, request: HttpRequest, *args: str, **kwargs: logging.Any) -> HttpResponse:
-    #     return super().post(request, *args, **kwargs)
+        new_entry.save()
+
+        # Save text-paste to blob store:
+        self.s3_service.uload_to_s3(s3_key=form.compose_key(), text_input=text)
+
+        full_url = f'/paste/{new_entry.url}/'
+        return JsonResponse({'message': 'Text saved successfully', 'url': full_url})
          
-class SubmitTextService_MixinStatic():
+class SubmitTextService():
         # Constants for error messages
     ERROR_HASH_SERVER = 'Hash-server error'
     ERROR_CREDENTIALS = 'Credentials error'
@@ -46,16 +68,27 @@ class SubmitTextService_MixinStatic():
     ERROR_MISSING_DATA = 'Missing frontend data'
     ERROR_INVALID_METHOD = 'Invalid request method'
 
-    @staticmethod
-    def get_hash_from_server(HASH_SERVER_URI):
-        response = requests.get(HASH_SERVER_URI)
-        # response.raise_for_status()
-        return response.json().get('hash')
+    def __init__(self, hash_server_uri) -> None:
+        self.hash_server_uri = hash_server_uri
+
+    def get_hash_from_server(self):
+        try:
+            response = requests.get(self.hash_server_uri)
+            response.raise_for_status()
+            return response.json().get('hash')
+        except Exception:
+            raise Exception(self.ERROR_HASH_SERVER)
     
     @staticmethod
-    def convert_string_to_unix_time(expiry_str) -> float:
+    def convert_string_to_unix_time(time_str) -> float:
         ''' Converts {days}.{hours}.{minutes} to unix time'''
         # Parse the expiry string
-        days, hours, minutes = map(int, expiry_str.split('.'))
+        days, hours, minutes = map(int, time_str.split('.'))
         time_til_expiry = datetime.timedelta(days=days, hours=hours, minutes=minutes)
         return float(time_til_expiry.timestamp())
+    
+    @staticmethod
+    def convert_iso_to_unix(time_iso8601):
+        parsed_t = dp.parse(time_iso8601)
+        t_in_seconds = parsed_t.timestamp()
+        return float(t_in_seconds)
