8e57e83b695097079cf6433b2bb8560b25cf9c4e
Nick <snicky5746@gmail.com>
2024-06-09 10:33:39 +0200

    s3_handler class implemented, working on views and forms

--- main_app/pastebin_main_app/utils/s3_handler.py
+++ main_app/pastebin_main_app/utils/s3_handler.py
@@ -14,31 +14,37 @@ class S3Service():
     ERROR_GENERIC = 'An error occurred'
 
     def __init__(self, aws_access_key_id, aws_secret_access_key, bucket_name) -> None:
-        self.s3 = boto3.client('s3',
-                               aws_access_key_id,
-                               aws_secret_access_key)
+        self.s3_client=boto3.client('s3',
+                aws_access_key_id=aws_access_key_id,
+                aws_secret_access_key=aws_secret_access_key)
         self.bucket_name = bucket_name
-    
+
+    def check_connectivity(self):
+        try:
+            self.s3_client.list_buckets()
+        except ClientError:
+            raise
+            
     def upload_to_s3(self, s3_key, text_input):
         try:
-            self.s3.put_object(Bucket=self.bucket_name, Key=str(s3_key), Body=str(text_input))
+            self.s3_client.put_object(Bucket=self.bucket_name, Key=str(s3_key), Body=str(text_input))
             LOGGER.info('Object uploaded to S3')
         except (NoCredentialsError, PartialCredentialsError) as e:
-            LOGGER.error(f'{S3Service.ERROR_CREDENTIALS}: {e}')
+            LOGGER.error(f'{self.__class__.ERROR_CREDENTIALS}: {e}')
             raise
         except ClientError as e:
-            LOGGER.error(f'{S3Service.ERROR_CLIENT}: {e}')
+            LOGGER.error(f'{self.__class__.ERROR_CLIENT}: {e}')
             raise
         except Exception as e:
-            LOGGER.error(f'{S3Service.ERROR_GENERIC}: {e}')
+            LOGGER.error(f'{self.__class__.ERROR_GENERIC}: {e}')
             raise
 
     def retrieve_from_s3(self, s3_key):
-        s3_object = self.s3.get_object(Bucket=self.bucket_name, Key=s3_key)
+        s3_object = self.s3_client.get_object(Bucket=self.bucket_name, Key=s3_key)
         return s3_object['Body'].read().decode('utf-8')
 
     def delete_from_s3(self, s3_key):
-        self.s3.delete_object(Bucket=self.bucket_name, Key=s3_key)
+        self.s3_client.delete_object(Bucket=self.bucket_name, Key=s3_key)
         LOGGER.info('Object deleted')
 
 key_id = environ.get('aws_access_key_id')
@@ -46,3 +52,4 @@ secret_key = environ.get('aws_secret_access_key')
 BUCKET_NAME=environ.get('BUCKET_NAME')
 
 myS3Service = S3Service(key_id, secret_key, BUCKET_NAME)
+myS3Service.check_connectivity()
--- main_app/pastebin_main_app/views.py
+++ main_app/pastebin_main_app/views.py
@@ -1,32 +1,33 @@
-import dateutil.parser as dp
-from django.forms import BaseModelForm
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.edit import CreateView
-from submit_text.submition_form import PasteSubmissionForm
+from django.views.generic import TemplateView
+from pastebin_main_app.submit_text.submition_form import PasteSubmissionForm
 from django.contrib.auth.hashers import make_password
-from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import logging
-from main_app.pastebin_main_app.utils.s3_handler import upload_to_s3
-import requests
-from os import environ
-from pastebin_main_app.apps import newExpiryController
-import datetime
 from models import Metadata
-from .. import SubmitTextService_MixinStatic
+
+@csrf_exempt
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = PasteSubmissionForm()
+        return context
 
 @csrf_exempt
 class SubmitTextView(CreateView):
     model = Metadata
-    template_name='home.html'
     form_class = PasteSubmissionForm
-    success_url = ...
 
-    def __init__(self, submit_text_service_object, s3_service, **kwargs: logging.Any) -> None:
+    def __init__(self, submit_text_service_object, s3_service, expiry_controller, **kwargs: logging.Any) -> None:
         super().__init__(**kwargs)
         self.submit_text_service=submit_text_service_object
         self.s3_service = s3_service
+        self.expiry_controller = expiry_controller
 
     def form_valid(self, form) -> HttpResponse:
         text = form.cleaned_data.get('text')
@@ -56,39 +57,11 @@ class SubmitTextView(CreateView):
         # Save text-paste to blob store:
         self.s3_service.uload_to_s3(s3_key=form.compose_key(), text_input=text)
 
+        # Add paste to expiry registry:
+        self.expiry_controller.add_event(expiry_time, self.__class__.model.id)        
+
         full_url = f'/paste/{new_entry.url}/'
+
         return JsonResponse({'message': 'Text saved successfully', 'url': full_url})
-         
-class SubmitTextService():
-        # Constants for error messages
-    ERROR_HASH_SERVER = 'Hash-server error'
-    ERROR_CREDENTIALS = 'Credentials error'
-    ERROR_CLIENT = 'Client error'
-    ERROR_GENERIC = 'An error occurred'
-    ERROR_MISSING_DATA = 'Missing frontend data'
-    ERROR_INVALID_METHOD = 'Invalid request method'
-
-    def __init__(self, hash_server_uri) -> None:
-        self.hash_server_uri = hash_server_uri
-
-    def get_hash_from_server(self):
-        try:
-            response = requests.get(self.hash_server_uri)
-            response.raise_for_status()
-            return response.json().get('hash')
-        except Exception:
-            raise Exception(self.ERROR_HASH_SERVER)
-    
-    @staticmethod
-    def convert_string_to_unix_time(time_str) -> float:
-        ''' Converts {days}.{hours}.{minutes} to unix time'''
-        # Parse the expiry string
-        days, hours, minutes = map(int, time_str.split('.'))
-        time_til_expiry = datetime.timedelta(days=days, hours=hours, minutes=minutes)
-        return float(time_til_expiry.timestamp())
-    
-    @staticmethod
-    def convert_iso_to_unix(time_iso8601):
-        parsed_t = dp.parse(time_iso8601)
-        t_in_seconds = parsed_t.timestamp()
-        return float(t_in_seconds)
+    def form_invalid(self):
+        return JsonResponse({'error': 'Invalid form data'}, status=400)
