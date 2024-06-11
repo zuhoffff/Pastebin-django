38a0a7122ab46933952bb3632dab8c4eae0d135a
Nick <snicky5746@gmail.com>
2024-06-11 13:43:52 +0200

    Working on password feature with cbvs

--- main_app/pastebin_main_app/urls.py
+++ main_app/pastebin_main_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 # from .get_text.get_text import get_text
 from pastebin_main_app.submit_text.submit_text_view import SubmitTextView
-from .get_text.get_text_view import PasswordPromptView, PasteDetailView
+from .get_text.get_text_view import PasswordPromptView, PasteDetailView, CheckProtection
 
 urlpatterns = [
     # path('', HomeView.as_view(), name='home'),
     path('', SubmitTextView.as_view(), name='submit_text'),
-    path('paste/<str:slug>/',PasteDetailView.as_view(), name='paste_detail_view'),
+    path('paste/<str:slug>/',CheckProtection.as_view(), name='check_protection'),
     path('paste/<str:slug>/password/', PasswordPromptView.as_view(), name='paste_password_prompt'),
+    path('paste/<str:slug>/view/',PasteDetailView.as_view(), name='paste_detail_view'),
 ]
