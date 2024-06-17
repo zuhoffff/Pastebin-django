301587a419d138c8a0f746045767add0b2dc92a6
Nick <snicky5746@gmail.com>
2024-06-17 13:15:05 +0200

    Handled invalid password and object not exist

--- main_app/pastebin_main_app/get_text/get_text_view.py
+++ main_app/pastebin_main_app/get_text/get_text_view.py
@@ -1,4 +1,4 @@
-from django.http.response import HttpResponse as HttpResponse
+from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.hashers import check_password
 from ..models import Metadata
@@ -12,20 +12,21 @@ from django.forms.models import model_to_dict
 
 logger = logging.getLogger(__name__)
 
-# TODO: implement session and authorisation
-
 class CheckProtection(View):
     model = Metadata
 
     def get(self, request, slug):
-        self.obj = get_object_or_404(self.model, slug=slug)
+        try:
+            self.obj=self.model.objects.get(slug=slug)
+        except self.model.DoesNotExist:
+            logger.info('Object does not exist. ')
         if self.obj.is_protected():
             logger.info('Text paste is protected: redirecting')
             return redirect('paste_password_prompt', slug=slug)
         else:
             logger.info('Text paste is public: redirecting to content')
-            return redirect('paste_detail_view',slug=slug)
-        
+            return redirect('paste_detail_view', slug=slug)
+
 class PasswordPromptView(View):
     model = Metadata
 
@@ -37,11 +38,12 @@ class PasswordPromptView(View):
         password = request.POST.get('password')
         if check_password(password, self.obj.password):
             logger.info('Password correct')
+            # Set a session flag
+            request.session[f'passed_protection_{slug}'] = True
             return redirect('paste_detail_view', slug=slug)
         else:
-            logger.info('bad password')
-            return render(request, 'password_prompt.html', {'slug': slug})
-
+            logger.info('Bad password')
+            return JsonResponse({'prompt_again': True})
 
 class PasteDetailView(View):
     model = Metadata
@@ -49,15 +51,21 @@ class PasteDetailView(View):
     payload = {}
 
     def get(self, request, slug) -> HttpResponse:
-        # Check if cached:
+        # Verify session flag for protected content
+        obj = get_object_or_404(self.model, slug=slug)
+        if obj.is_protected() and not request.session.get(f'passed_protection_{slug}'):
+            logger.info('Access denied: Password protection not passed')
+            return redirect('paste_password_prompt', slug=slug)
+        
+        # Check if cached
         self.payload = cache.get(slug)
         if not self.payload:
-            logger.info('hit db')
-            self.obj=get_object_or_404(self.model, slug=slug)
-            self.payload=model_to_dict(self.obj)
-            self.payload['text']=myS3Service.retrieve_from_s3(self.obj.slug)
-            # Convert expiry time to convinient format
+            logger.info('Hit DB')
+            self.obj = obj
+            self.payload = model_to_dict(self.obj)
+            self.payload['text'] = myS3Service.retrieve_from_s3(self.obj.slug)
+            # Convert expiry time to convenient format
             cache.set(slug, self.payload, timeout=self.cache_timeout)
         else:
-            logger.info('hit cache')
+            logger.info('Hit cache')
         return render(request, 'view_paste.html', self.payload)
--- main_app/pastebin_main_app/slugForm.py
+++ main_app/pastebin_main_app/slugForm.py
@@ -1,4 +1,11 @@
 from django import forms
+from .models import Metadata
 
 class SlugForm(forms.Form):
     slug = forms.SlugField(label='Enter the identifier of a text-paste')
+
+    # Check if obj exists
+    def check_if_exists(self):
+        if Metadata.objects.filter(slug = self.cleaned_data['slug']).exists():
+            return True
+        return False
