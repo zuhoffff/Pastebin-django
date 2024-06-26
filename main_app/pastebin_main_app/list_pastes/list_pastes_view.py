87f6aeeac33561fd3ffe7a55ca18ae78a63c63c7
Nick <snicky5746@gmail.com>
2024-06-26 21:00:44 +0200

    Added chart of all text pastes

--- main_app/pastebin_main_app/list_pastes/list_pastes_view.py
+++ main_app/pastebin_main_app/list_pastes/list_pastes_view.py
@@ -5,6 +5,12 @@ from django.http import HttpResponse, JsonResponse
 from django.views.generic import ListView
 from django.urls import reverse
 from pastebin_main_app.models import Metadata
+from django.shortcuts import render
+import json
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 class ListPastes(ListView):
     model=Metadata
@@ -12,4 +18,12 @@ class ListPastes(ListView):
     context_object_name = 'metadatas'
 
     def get_queryset(self):
-        return self.model.objects.all()
+        self.fields = ['name', 'slug', 'author', 'timestamp', 'expiry_time']
+        queryset = self.model.objects.values(*self.fields)
+        logger.info(list(queryset))
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['fields'] = self.fields
+        return context
